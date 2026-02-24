06e6beefa22cd63fd2dcc9c38161bc09fdfa689c
austinlin11562 <austinlin11562@gmail.com>
2026-02-24 15:51:19 -0500

    Added intro to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Austin Lin" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi! My name is Austin and I'm a sophomore from Massachusetts. I'm interested in computer science, robotics, and machine learning. In my free time I enjoy swimming, hiking, playing chess, and working on personal projects. Currently, I am working on a monitoring system for my dog using a Raspberry Pi and some microcontrollers. Hopefully I can integrate what I learn from this course into my current and future projects!")
 
 if __name__ == "__main__":
     main()
